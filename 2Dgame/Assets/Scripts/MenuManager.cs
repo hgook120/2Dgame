d0821590d106d5ceee0e1832a7fadfc84bdfe81f
hgook120 <55180627+hgook120@users.noreply.github.com>
2020-10-27 16:00:57 +0800

    20201027

---
+++ 2Dgame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // 註解：說明
+    // 按鈕如何與程式溝通
+    // 需要：方法 (函式、功能) - 程式功能、演算法
+    // 語法：
+    // 類型 方法名稱 () {}
+    // void 無類型
+    void StartGame()
+    {
+
+    }
+
+    void QuitGame()
+    {
+
+    }
+}
